c6d55a72cbe31d4ab8b8940f58888388c7fd841d
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2020-12-14 23:58:10 +0100

    Same execution time for the list and IEnumerable

--- EnumerableCollections/EnumerableCollections.Tests/EnumerableTest.cs
+++ EnumerableCollections/EnumerableCollections.Tests/EnumerableTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NFluent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace EnumerableCollections.Tests
@@ -46,11 +47,56 @@ namespace EnumerableCollections.Tests
             Check.That(partNames).ContainsExactly("Fatou", "Jackito", "Ritom");
         }
 
+        [TestMethod]
+        public void Should_have_same_execution_time_with_list_and_Enumerable_one()
+        {
+            var nameList = GetNames();
+            var names = GetEnumerableNames();
+            var result = new List<string> { "Fatou", "Jackito", "Ritom" };
+            const string containgValue = "to";
+
+            var listWatch = Stopwatch.StartNew();
+            IEnumerable<string> partNameList = nameList.Where(n => n.Contains(containgValue));
+            foreach (var item in partNameList)
+            {
+                result.Contains(item);
+            }
+            listWatch.Stop();
+            var executionTimeUsingList = listWatch.ElapsedMilliseconds;
+
+            var enumerableWatch = Stopwatch.StartNew();
+            IEnumerable<string> partNames = names.Where(n => n.Contains(containgValue));
+            foreach (var item in partNames)
+            {
+                result.Contains(item);
+            }
+            enumerableWatch.Stop();
+            var executionTimeUsingEnumerable = enumerableWatch.ElapsedMilliseconds;
+
+            Check.That(executionTimeUsingEnumerable).IsEqualTo(executionTimeUsingList);
+        }
+
         private List<string> GetNames()
         {
             return new List<string> {"Aline", "Tom", "Fen", "Fatou",
                 "Amine", "Jackito", "Ritom", "Yosr", "Cyril", "Olfa",
                 "Mathilde", "Karim"};
+        }
+
+        private IEnumerable<string> GetEnumerableNames()
+        {
+            yield return "Aline";
+            yield return "Tom";
+            yield return "Fen";
+            yield return "Fatou";
+            yield return "Amine";
+            yield return "Jackito";
+            yield return "Ritom";
+            yield return "Yosr";
+            yield return "Cyril";
+            yield return "Olfa";
+            yield return "Mathilde";
+            yield return "Karim";
         }
     }
 }
