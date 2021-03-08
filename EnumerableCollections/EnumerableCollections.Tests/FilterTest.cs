107d1060b48e3ae331c3c96d1a5573dc42229d78
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2021-03-08 21:30:27 +0100

    filter test

---
+++ EnumerableCollections/EnumerableCollections.Tests/FilterTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnumerableCollections.Tests
+{
+    [TestClass]
+    public class FilterTest
+    {
+        [TestMethod]
+        public void Linq_Filter_Order_IEnumerable()
+
+        {
+            IEnumerable<string> enumerable = new List<string>() {
+                "Cyril",
+                "Olfa",
+                "Mathilde",
+                "Karim",
+            };
+
+            var filteredAndOrdered =
+                from person in enumerable
+                where person.Contains("i")
+                orderby person
+                select person;
+
+            Check.That(filteredAndOrdered)
+                 .ContainsExactly("Cyril", "Karim", "Mathilde");
+        }
+
+        [TestMethod]
+        public void Linq_Method_Filter_Order_IEnumerable()
+        {
+            IEnumerable<string> enumerable = new List<string>() {
+                "Cyril",
+                "Olfa",
+                "Mathilde",
+                "Karim",
+            };
+
+            var filteredAndOrdered = enumerable
+                .Where(person => person.Contains("i"))
+                .OrderBy(person => person);
+
+            Check.That(filteredAndOrdered)
+                .ContainsExactly("Cyril", "Karim", "Mathilde");
+        }
+    }
+}
