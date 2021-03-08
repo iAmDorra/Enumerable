2af31d3838607a747942c79df6729ba0a222d00b
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2021-03-08 22:23:24 +0100

    Add order descending

--- EnumerableCollections/EnumerableCollections.Tests/OrderTest.cs
+++ EnumerableCollections/EnumerableCollections.Tests/OrderTest.cs
@@ -45,5 +45,42 @@ namespace EnumerableCollections.Tests
                 new Person(2, "Olfa"),
                 new Person(4, "Tom"));
         }
+
+        [TestMethod]
+        public void Order_descending_collection_with_primitive_type()
+        {
+            IEnumerable<string> names = new List<string>() {
+                "Arnauld",
+                "Olfa",
+                "Mathilde",
+                "Tom"
+            };
+
+            var orderedNames = names.OrderByDescending(name => name);
+
+            Check.That(orderedNames)
+                 .ContainsExactly("Tom", "Olfa", "Mathilde", "Arnauld");
+        }
+
+        [TestMethod]
+        public void Order_descending_collection_with_objects()
+        {
+            IEnumerable<Person> persons = new List<Person>()
+            {
+                new Person(4, "Tom"),
+                new Person(3, "Arnauld"),
+                new Person(1, "Arnauld"),
+                new Person(2, "Olfa"),
+            };
+
+            var orderedNames = persons.OrderByDescending(person => person.Name);
+
+            Check.That(orderedNames)
+                .ContainsExactly(
+                new Person(4, "Tom"),
+                new Person(2, "Olfa"),
+                new Person(3, "Arnauld"),
+                new Person(1, "Arnauld"));
+        }
     }
 }
