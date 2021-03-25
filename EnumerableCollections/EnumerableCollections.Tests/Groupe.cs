fc7b2a1988957b7ac75b29c32c46e84ece5b9752
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2021-03-25 21:18:24 +0100

    Add GroupJoin tests

---
+++ EnumerableCollections/EnumerableCollections.Tests/Groupe.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace EnumerableCollections.Tests
+{
+    internal class Groupe
+    {
+        private int id;
+        private IEnumerable<Person> childs;
+
+        public Groupe(int id, IEnumerable<Person> childs)
+        {
+            this.id = id;
+            this.childs = childs;
+        }
+    }
+}
--- EnumerableCollections/EnumerableCollections.Tests/JoinTest.cs
+++ EnumerableCollections/EnumerableCollections.Tests/JoinTest.cs
@@ -33,5 +33,29 @@ namespace EnumerableCollections.Tests
                 new Child(4, "Céline", "Tom"),
                 new Child(3, "Nicolas", "Arnauld"));
         }
+
+        [TestMethod]
+        public void Should_group_join_collections()
+        {
+            IEnumerable<Person> persons = new List<Person>
+            {
+                new Person(4, "Tom"),
+                new Person(3, "Arnauld"),
+                new Person(1, "Laurent"),
+                new Person(1, "Alice"),
+                new Person(2, "Olfa"),
+            };
+            IEnumerable<Parent> parents = new List<Parent>
+            {
+                new Parent(1,"Anne"),
+                new Parent(4,"Céline"),
+                new Parent(3,"Nicolas")
+            };
+
+            var result = parents.GroupJoin(persons, parent => parent.Id, person => person.Id, (parent, childs) => new Groupe(parent.Id, childs))
+                .ToList();
+
+            Check.That(result).CountIs(3);
+        }
     }
 }
