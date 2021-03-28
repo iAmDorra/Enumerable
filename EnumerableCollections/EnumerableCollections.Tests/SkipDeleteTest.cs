de3fe91765e00bdbdaa3456da8dfff734f25ec68
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2021-03-29 00:22:26 +0200

    use case skip delete

---
+++ EnumerableCollections/EnumerableCollections.Tests/SkipDeleteTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnumerableCollections.Tests
+{
+    [TestClass]
+    public class SkipDeleteTest
+    {
+        private List<Person> persons;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            persons = GetPerons();
+        }
+
+        [TestMethod]
+        public void Should_return_less_elements_than_expected()
+        {
+            var newPersonSequence = persons.Skip(1);
+            // Waiting to skip only Person(2,"Lucie") 
+            // => newPersonSequence should contains Person(4, "Laura") and Person(1, "Arnaud")
+            RemoveElement();
+
+            // newPersonSequence is not calculated yet, so the remove should be applicated before
+            Check.That(newPersonSequence.Count()).IsEqualTo(1);
+            Check.That(newPersonSequence).Contains(new Person(1, "Arnaud"));
+        }
+
+        private void RemoveElement()
+        {
+            persons.RemoveAt(0);
+        }
+
+        private static List<Person> GetPerons()
+        {
+            return new List<Person>
+            {
+                new Person(2, "Lucie"),
+                new Person(4, "Laura"),
+                new Person(1, "Arnaud")
+            };
+        }
+    }
+}
